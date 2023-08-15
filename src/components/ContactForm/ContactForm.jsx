fd8e6a13ed5a4f141af157155dab76d3d3921159
rasinoleg <rasinoleg3@gmail.com>
2023-08-15 22:12:07 +0300

    react 3 phonebook

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styles from './ContactForm.module.css';
+
+
+export default class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    if (this.state.name.length === 0 || this.state.number.length === 0) {
+      alert('Fields must be filled!');
+      return;
+    }
+      this.props.onAddContact(this.state);
+      this.setState({ name: '', number: '' });
+    
+  };
+  render() {
+    return (
+      <form className={styles.TaskEditor} onSubmit={this.handleSubmit}>
+        <label className={styles.TaskEditor_label}>
+          Name
+          <input
+            className={styles.TaskEditor_input}
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label className={styles.TaskEditor_label}>
+          Number
+          <input
+            className={styles.TaskEditor_input}
+            type="text"
+            name="number"
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+        </label>
+        <button className={styles.TaskEditor_button} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onAddContact: PropTypes.func.isRequired,
+};
