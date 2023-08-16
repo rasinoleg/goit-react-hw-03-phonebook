035513be680fdf3b194a19496556a8c3e5d4974d
rasinoleg <rasinoleg3@gmail.com>
2023-08-16 11:53:15 +0300

    react 3 phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,69 @@
-import React, { Component } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import ContactList from './ContactList/ContactList';
-import Filter from './Filter/Filter';
-import ContactForm from './ContactForm/ContactForm';
+import React, { Component } from "react";
+import { v4 as uuidv4 } from "uuid";
+import ContactList from "./ContactList/ContactList";
+import Filter from "./Filter/Filter";
+import ContactForm from "./ContactForm/ContactForm";
 
 export class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: '',
+    filter: "",
   };
+  
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts'); 
+    const parsedContacts = JSON.parse(contacts); 
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts }); 
+    }
+  }
 
-  addContact = data => {
-    const { name, number } = data;
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
-    const contactExists = this.state.contacts.find(
-      cont => cont.name === name
-    );
+  addContact = (task) => {
+    const searchSameName = this.state.contacts
+      .map((cont) => cont.name)
+      .includes(task.name);
 
-    if (contactExists) {
-      alert(`${name} is already in contacts`);
+    if (searchSameName) {
+      alert(`${task.name} is already in contacts`);
+    } else if (task.name.length === 0) {
+      alert("Fields must be filled!");
     } else {
       const contact = {
-        name,
-        number,
+        ...task,
         id: uuidv4(),
       };
 
-      this.setState(prevState => ({
+      this.setState((prevState) => ({
         contacts: [...prevState.contacts, contact],
       }));
     }
   };
 
-  changeFilter = filter => {
+  changeFilter = (filter) => {
     this.setState({ filter });
   };
 
   getVisibleContacts = () => {
     const { contacts, filter } = this.state;
 
-    return contacts.filter(contacts =>
+    return contacts.filter((contacts) =>
       contacts.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  removeContact = contactId => {
-    this.setState(prevState => {
+  removeContact = (contactId) => {
+    this.setState((prevState) => {
       return {
         contacts: prevState.contacts.filter(({ id }) => id !== contactId),
       };
@@ -68,7 +81,9 @@ export class App extends Component {
 
         <ContactForm onAddContact={this.addContact} />
         <h2>Contacts</h2>
+        {visibleContacts.length > 1 && (
           <Filter value={filter} onChangeFilter={this.changeFilter} />
+        )}
         {visibleContacts.length > 0 && (
           <ContactList
             contacts={visibleContacts}
@@ -79,3 +94,5 @@ export class App extends Component {
     );
   }
 }
+
+
